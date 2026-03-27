c36d85a26d1dcb86bc3157500fcab158dd899532
Thomaz de aquino collet e silva <tcollet98@hotmail.com>
2026-03-26 22:43:19 -0300

    initial commit

---
+++ text-translator-api/src/main/java/com/thomazcollet/text_translator_api/TextTranslatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.thomazcollet.text_translator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextTranslatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextTranslatorApiApplication.class, args);
+	}
+
+}
---
+++ text-translator-api/src/test/java/com/thomazcollet/text_translator_api/TextTranslatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomazcollet.text_translator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextTranslatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
